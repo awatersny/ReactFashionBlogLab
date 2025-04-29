f4127fcddfb7af4d50bddd87cc664a743a9f5764
awatersny <awatersny@gmail.com>
2025-04-29 00:34:23 -0400

    Switch from typescript to js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import BlogPost from './components/BlogPost'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+      <BlogPost/>
+    </div>
+  )
+}
+
+export default App
